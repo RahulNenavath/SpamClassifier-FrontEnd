f5503efadb7bba8bfa71cddc2ee326e245776bea
Rahul_nenavath <rahul.nenavath@cloudsek.com>
2020-09-11 00:30:14 +0530

    refactored code

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,16 +2,19 @@ import firebase  from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firebase-firestore';
 import 'firebase/storage';
+import apiData from "./firebaseAPI.json";
 
-const config = {
-    apiKey: "AIzaSyDY_tcZRdaMceRBXUsS4dsywvy0fc9Jl78",
-    authDomain: "spam-classifier-b4a27.firebaseapp.com",
-    databaseURL: "https://spam-classifier-b4a27.firebaseio.com",
-    projectId: "spam-classifier-b4a27",
-    storageBucket: "spam-classifier-b4a27.appspot.com",
-    messagingSenderId: "674091040076",
-    appId: "1:674091040076:web:b1a36344ba0eb0a586a3e9"
-};
+// const config = {
+//     apiKey: "AIzaSyDY_tcZRdaMceRBXUsS4dsywvy0fc9Jl78",
+//     authDomain: "spam-classifier-b4a27.firebaseapp.com",
+//     databaseURL: "https://spam-classifier-b4a27.firebaseio.com",
+//     projectId: "spam-classifier-b4a27",
+//     storageBucket: "spam-classifier-b4a27.appspot.com",
+//     messagingSenderId: "674091040076",
+//     appId: "1:674091040076:web:b1a36344ba0eb0a586a3e9"
+// };
+
+const config = apiData
 
 class Firebase{
     constructor(){
