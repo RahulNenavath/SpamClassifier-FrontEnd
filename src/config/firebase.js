f9f9ebfb8aed72d556f5705c166ae486a7734d7f
Rahul_nenavath <rahul.nenavath@cloudsek.com>
2020-09-11 10:26:11 +0530

    fixed bug

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,9 +2,16 @@ import firebase  from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firebase-firestore';
 import 'firebase/storage';
-import apiData from "./firebaseAPI.json"; // Firebase API Key
 
-const config = apiData
+const config = {
+    "apiKey": process.env.REACT_APP_FIREBASE_APIKEY,
+    "authDomain": process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+    "databaseURL": process.env.REACT_APP_FIREBASE_DATABASEURL,
+    "projectId": process.env.REACT_APP_FIREBASE_PROJECTID,
+    "storageBucket": process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+    "messagingSenderId": process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+    "appId": process.env.REACT_APP_FIREBASE_APPID
+}
 
 class Firebase{
     constructor(){
