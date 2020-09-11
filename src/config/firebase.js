8cf5cf666ce868ce500503cc6df1077d23e54996
Rahul_nenavath <rahul.nenavath@cloudsek.com>
2020-09-11 12:14:59 +0530

    changed config file

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,16 +2,19 @@ import firebase  from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firebase-firestore';
 import 'firebase/storage';
+import FirebaseConfig from "./firebaseConfig";
 
-const config = {
-    "apiKey": process.env.REACT_APP_FIREBASE_APIKEY,
-    "authDomain": process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
-    "databaseURL": process.env.REACT_APP_FIREBASE_DATABASEURL,
-    "projectId": process.env.REACT_APP_FIREBASE_PROJECTID,
-    "storageBucket": process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
-    "messagingSenderId": process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
-    "appId": process.env.REACT_APP_FIREBASE_APPID
-}
+// const config = {
+//     "apiKey": process.env.REACT_APP_FIREBASE_APIKEY,
+//     "authDomain": process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+//     "databaseURL": process.env.REACT_APP_FIREBASE_DATABASEURL,
+//     "projectId": process.env.REACT_APP_FIREBASE_PROJECTID,
+//     "storageBucket": process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+//     "messagingSenderId": process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+//     "appId": process.env.REACT_APP_FIREBASE_APPID
+// }
+
+const config = FirebaseConfig
 
 class Firebase{
     constructor(){
